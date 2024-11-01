c34e897dcb3a936431fcd4dfe45fc06e96503996
KgosiSegale <u22534718@tuks.co.za>
2024-11-01 18:29:06 +0200

    Update PolicyCommand.h

--- PolicyCommand.h
+++ PolicyCommand.h
@@ -2,6 +2,8 @@
 #define POLICYCOMMAND_H
 
 #include "Command.h"
+#include "City.h"
+#include <string>
 
 /**
  * @brief Command to handle the implementation of government policies.
@@ -9,6 +11,11 @@
  * ConcreteCommand participant in the Command design pattern. It implements the `execute()` method to handle the enforcement of government policies.
  */
 class PolicyCommand : public Command {
+private:
+    City* city;
+    std::string policyName;
+    int previousHappiness;
+
 public:
     /**
      * @brief Constructs a new PolicyCommand object.
@@ -24,6 +31,17 @@ public:
      * @brief Executes the command to implement a policy.
      */
     void execute() override;
+
+    /**
+     * @brief Undo functionality.
+     */
+    void undo() override;
+
+    /**
+     * @brief Redo functionality.
+     */
+    void redo() override;
+    
 };
 
 #endif // POLICYCOMMAND_H
