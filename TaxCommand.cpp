d8c9b7904b589c412020c500736ee335be634360
KgosiSegale <u22534718@tuks.co.za>
2024-11-01 18:25:31 +0200

    Update TaxCommand.cpp

--- TaxCommand.cpp
+++ TaxCommand.cpp
@@ -3,13 +3,31 @@
 #include <iostream>
 
 TaxCommand::TaxCommand(TaxAuthority* taxAuthority, int taxRate)
-    : taxAuthority(taxAuthority), taxRate(taxRate) {}
+    : taxAuthority(taxAuthority), taxRate(taxRate), previousTaxRate(0) {}
 
-TaxCommand::~TaxCommand() {
-    // Cleanup resources if necessary
-}
+TaxCommand::~TaxCommand() {}
 
 void TaxCommand::execute() {
+    Command::execute(); // Update status
+
+    // Store current tax rate and apply the new one
+    previousTaxRate = taxAuthority->getCurrentTaxRate();
     taxAuthority->adjustTaxRate(taxRate);
     std::cout << "Adjusted tax rate to " << taxRate << "%" << std::endl;
+}
+
+void TaxCommand::undo() {
+    Command::undo();  // Update status
+
+    // Revert to the previous tax rate
+    taxAuthority->adjustTaxRate(previousTaxRate);
+    std::cout << "Reverted tax rate to " << previousTaxRate << "%" << std::endl;
+}
+
+void TaxCommand::redo() {
+    Command::redo();  // Update status
+
+    // Reapply the adjusted tax rate
+    taxAuthority->adjustTaxRate(taxRate);
+    std::cout << "Re-applied tax rate of " << taxRate << "%" << std::endl;
 }
