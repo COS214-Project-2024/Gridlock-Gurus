5c9907836038b1095ccdffd519513ade2f88ef9d
Kc-Nell <kirstennell01@gmail.com>
2024-11-04 14:04:03 +0200

    H - L headers done

--- headers/HighFundingState.h
+++ headers/HighFundingState.h
@@ -4,12 +4,40 @@
 #include "HealthService.h"
 #include "HealthState.h"
 #include <string>
+
+/**
+ * @class HighFundingState
+ * @brief Represents a state of high funding for the health service.
+ */
 class HighFundingState : public HealthState {
 public:
+    /**
+     * @brief Constructs a HighFundingState.
+     * @param service Reference to the associated HealthService.
+     */
     HighFundingState(HealthService& service) : HealthState(service) {}
-     ~HighFundingState() override = default;
+
+    /**
+     * @brief Destructor for HighFundingState.
+     */
+    ~HighFundingState() override = default;
+
+    /**
+     * @brief Gets the health status in the high funding state.
+     * @return The health status as a string.
+     */
     std::string getHealthStatus() override;
+
+    /**
+     * @brief Gets the quality of time spent in the health service.
+     * @return The quality of time as an integer.
+     */
     int getQualityOfTime() override;
+
+    /**
+     * @brief Gets the name of the high funding state.
+     * @return The name of the state.
+     */
     std::string getName() override;
 };
 
