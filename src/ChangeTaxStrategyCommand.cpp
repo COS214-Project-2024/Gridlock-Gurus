6f0686582d63002323d55b19b2b278f6da2ae51f
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-11-04 16:10:12 +0200

    Update ChangeTaxStrategyCommand.cpp

--- src/ChangeTaxStrategyCommand.cpp
+++ src/ChangeTaxStrategyCommand.cpp
@@ -1,8 +1,5 @@
 #include "ChangeTaxStrategyCommand.h"
 
-/**
- * @brief Executes the command to change the tax strategy.
- */
 void ChangeTaxStrategyCommand::execute() {
     if(strategy == TaxType::Flat) {
         city->changeTaxStrategy(std::make_unique<FlatTaxStrategy>());
