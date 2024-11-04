97cf7b767b0b9c0ba29e2269ec810e7a712756ed
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-11-04 12:04:39 +0200

    Update Building.cpp

--- src/Building.cpp
+++ src/Building.cpp
@@ -40,6 +40,7 @@ std::string Building::getDetails() const {
     details += "Building Name:\t";
     details += name;
     details += "\nLocation:\t";
+    details += location;
     return details;
 }
 
