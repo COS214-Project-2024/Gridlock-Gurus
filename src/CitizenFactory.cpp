6dfe93b8e01da2d090ae7b0d2080199bb5091e81
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-11-04 16:10:57 +0200

    Update CitizenFactory.cpp

--- src/CitizenFactory.cpp
+++ src/CitizenFactory.cpp
@@ -1,20 +1,9 @@
 #include "CitizenFactory.h"
 #include <iostream>
 
-/**
- * @brief Constructs a CitizenFactory with a reference to the TaxAuthority.
- * @param taxAuthority Shared pointer to the TaxAuthority.
- */
 CitizenFactory::CitizenFactory(std::shared_ptr<TaxAuthority> taxAuthority) : taxAuthority(taxAuthority){}
 
 
-/**
- * @brief Creates a Citizen with the specified type and initial attributes.
- * @param type Type of citizen to create.
- * @param startingSatisfaction Initial satisfaction level.
- * @param startingFunds Initial funds available to the citizen.
- * @return Pointer to the newly created Citizen.
- */
 Citizen* CitizenFactory::createCitizen(CitizenType type, int startingSatisfaction, int startingFunds) {
     int id = citizenCount;
     citizenCount++;
