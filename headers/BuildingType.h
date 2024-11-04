82e4daeabd4cc1769395d04f11d594ab2a2aa117
PatV3-0 <130934721+PatV3-0@users.noreply.github.com>
2024-11-04 11:17:37 +0200

    Update BuildingType.h

--- headers/BuildingType.h
+++ headers/BuildingType.h
@@ -4,32 +4,39 @@
 #include <string>
 #include <ostream>
 
-/**
- * @brief Enumeration representing different types of buildings.
- */
 enum class BuildingType {
-    Residential,     ///< Residential building.
-    Shop,            ///< Shop or commercial building.
-    BrickFactory,    ///< Factory for producing bricks.
-    SteelFactory,    ///< Factory for producing steel.
-    WoodFactory,     ///< Factory for producing wood.
-    Bank,            ///< Bank building.
-    Flat,            ///< Apartment or flat.
-    Estate,          ///< Estate or large residence.
-    House,           ///< House or single residence.
-    Statue,          ///< Statue or monument.
-    Park,            ///< Public park.
-    PoliceStation,   ///< Police station.
-    Hospital,        ///< Hospital or healthcare facility.
-    School           ///< Educational institution.
+    Residential,
+    Shop,
+    BrickFactory,
+    SteelFactory,
+    WoodFactory,
+    Bank,
+    Flat,
+    Estate,
+    House,
+    Statue,
+    Park,
+    PoliceStation,
+    Hospital,
+    School
 };
 
-/**
- * @brief Overloads the output stream operator for BuildingType.
- * @param os Output stream.
- * @param type The BuildingType to output.
- * @return Reference to the modified output stream.
- */
-std::ostream& operator<<(std::ostream& os, BuildingType type);
+// Overload operator<< to allow printing of BuildingType
+/*std::ostream& operator<<(std::ostream& os, BuildingType type) {
+    switch (type) {
+        case BuildingType::Residential: os << "Residential"; break;
+        case BuildingType::Shop:        os << "Shop"; break;
+        case BuildingType::BrickFactory:os << "Brick Factory"; break;
+        case BuildingType::WoodFactory: os << "Wood Factory"; break;
+        case BuildingType::SteelFactory:os << "Steel Factory"; break;
+        case BuildingType::Office:      os << "Office"; break;
+        case BuildingType::Bank:        os << "Bank"; break;
+        case BuildingType::House:       os << "House"; break;
+        case BuildingType::Flat:        os << "Flat"; break;
+        case BuildingType::Estate:      os << "Estate"; break;
+        default:                        os << "Building"; break;
+    }
+    return os;
+}*/
 
 #endif // BUILDINGTYPE_H
