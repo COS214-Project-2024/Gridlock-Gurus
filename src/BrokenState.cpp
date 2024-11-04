0ac017a0c35be2fbbe46dc265c3e741a0367031e
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-11-04 16:08:11 +0200

    Update BrokenState.cpp

--- src/BrokenState.cpp
+++ src/BrokenState.cpp
@@ -1,16 +1,6 @@
-/**
-* @class BrokenState
- * @brief Definition of the BrokenState class, representing a non-functional state.
- */
-
 #include "BrokenState.h"
 #include <iostream>
 
-/**
- * @brief Executes the operation in the broken state.
- *
- * @return A boolean value indicating the success of the operation; always returns false.
- */
 bool BrokenState::run() {
     return false;
 }
